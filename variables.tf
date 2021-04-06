a4b12b7552ca4b5212c7d4c0d33d233619d74da9
Nicholas Gulrajani <nicholas.gulrajani@wwt.com>
2021-04-06 09:30:16 -0400

    Region Chg

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "region" {
   description = "This is the cloud hosting region where Terraform deploys your webapp"
-  default = "us-east-1"
+  default = "us-west-1"
   
 }
 
 variable "prefix" {
   description = "This is the environemnt where Terraform deploys your webapp"
-  default = "nicdev"
+  default = "dev"
 }
